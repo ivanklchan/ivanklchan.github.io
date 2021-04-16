e19d9ce43634dffd40df5d986155d3f78bba4466
ivanchan868@gmail.com <ivanchan868@gmail.com>
2021-04-15 22:45:04 -0400

    HTML update
    This minor HTML update fixes the previously error that jumps back to the index page whenever the toggle button is pressed on pages using the default.html template.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,6 @@ const TOGGLEBUTTON = document.getElementsByClassName('toggle-button')[0];
 const NAVBARLINKS = document.getElementsByClassName('navbar-links')[0];
 
 TOGGLEBUTTON.addEventListener('click', () => {
-    NAVBARLINKS.classList.toggle('active')
+    NAVBARLINKS.classList.toggle('active');
 });
 
