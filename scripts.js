1ba2d0846ee769960eb43ea390dca586d5c153df
ivanchan868@gmail.com <ivanchan868@gmail.com>
2021-04-20 22:53:55 -0400

    Light Mode Update
    This update adds a new functionality across all web pages to allow the user to toggle between dark and light mode.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,28 @@
 const TOGGLEBUTTON = document.getElementsByClassName('toggle-button')[0];
 const NAVBARLINKS = document.getElementsByClassName('navbar-links')[0];
+const THEMECHANGE = document.getElementById('theme-change');
 
+// Function to enable drop down toggle button when the viewport is smaller than a specific amount of pixels defined in css file. 
 TOGGLEBUTTON.addEventListener('click', () => {
     NAVBARLINKS.classList.toggle('active');
 });
 
+//Function to enable toggle between dark and light theme. 
+THEMECHANGE.addEventListener('click', checkMode);
+
+function lightModeOn(){
+    document.body.classList.add("light-mode");
+}
+ 
+function lightModeOff(){
+    document.body.classList.remove("light-mode");
+}
+
+function checkMode(){
+    if(THEMECHANGE.checked){
+        lightModeOn();
+    }
+    else{
+        lightModeOff();
+    }
+}
